e268954391ef332901c0b16400e1badc5200b8a9
David McElligott <damc.dev@gmail.com>
2015-03-09 14:25:51 -0400

    removed duplicate default node

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,18 +33,3 @@ File { backup => 'main' }
 # Kill deprecation warnings in PE 3.3:
 Package { allow_virtual => false }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-}
