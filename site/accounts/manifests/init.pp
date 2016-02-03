6ce2ad5c231cf3be585a9777326c6318f16b4518
David McElligott <damc.dev@gmail.com>
2016-02-03 12:01:32 -0500

    added authorized keys to accounts

--- site/accounts/manifests/init.pp
+++ site/accounts/manifests/init.pp
@@ -1,12 +1,16 @@
 class accounts {
   @accounts::virtual {'david':
-    uid      => 11111,
-    realName => 'David McElligott',
+    uid        => 11111,
+    realName   => 'David McElligott',
+    sshKey     => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFsMHpX+9b1fCr+IKKWG+3HoBA8ToN+CLZ2Eb3tvP1TZt/o5JVj4pmEFF9Um1f25RZIiZ4lGbm8kfliy6R+/WNh3Wm83e/qPwevD6DDPDN7/nyb5q2VzpkUj1Y0Ov21EwVs5nWwkQJrEn11qGjO7ndXPRR+48p4Xo6CpAulL5CD2yoCG/kZhUCf5ajIml4/99tEzFWpmdtDpbus7mXeLmxW9z5JhOHUuxMy0wqr1OeiYD0nYBErpOVIryHTz4LO0kSJbts2vNoORm7mjLmoVFLJpKltlFPQQAnfjsYlSgFVTMZUwZ6+CthKVAQXtsEbG0+YyQZHZzszmBJcxq/BQcl damc.dev@gmail.com',
+    sshKeyType => 'ssh-rsa',
   }  
   
   @accounts::virtual {'test_user':
     uid      => 99999,
     realName => 'Temporary User',
+    sshKey     => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFsMHpX+9b1fCr+IKKWG+3HoBA8ToN+CLZ2Eb3tvP1TZt/o5JVj4pmEFF9Um1f25RZIiZ4lGbm8kfliy6R+/WNh3Wm83e/qPwevD6DDPDN7/nyb5q2VzpkUj1Y0Ov21EwVs5nWwkQJrEn11qGjO7ndXPRR+48p4Xo6CpAulL5CD2yoCG/kZhUCf5ajIml4/99tEzFWpmdtDpbus7mXeLmxW9z5JhOHUuxMy0wqr1OeiYD0nYBErpOVIryHTz4LO0kSJbts2vNoORm7mjLmoVFLJpKltlFPQQAnfjsYlSgFVTMZUwZ6+CthKVAQXtsEbG0+YyQZHZzszmBJcxq/BQcl damc.dev@gmail.com',
+    sshKeyType => 'ssh-rsa',
   }
 
 }
--- site/accounts/manifests/virtual.pp
+++ site/accounts/manifests/virtual.pp
@@ -1,4 +1,4 @@
-define accounts::virtual ($uid,$realName) {
+define accounts::virtual ($uid,$realName,$sshKey,$sshKeyType) {
   user { $title:
     ensure            =>  'present',
     uid               =>  $uid,
@@ -20,5 +20,44 @@ define accounts::virtual ($uid,$realName) {
     group             =>  $title,
     mode              =>  0750,
     require           =>  [ User[$title], Group[$title] ],
+  }
+
+  if ($sshKey != '') {
+    file { "/software/ssh/${title}":
+      ensure  => directory,
+      owner             =>  $title,
+      group             =>  $title,
+      mode              =>  0750,
+      require           =>  [ User[$title], Group[$title], File["/software/ssh"] ],
+    }
+
+    ssh_authorized_key { $title:
+      target  => "/software/ssh/${title}/authorized_keys",
+      ensure  => present,
+      name    => $title,
+      user    => $title,
+      type    => $sshKeyType,
+      key     => $sshKey,
+      require => File["/software/ssh/${title}"],
+    }
+
+
+    file { "/home/${title}/.ssh":
+      ensure  => directory,
+      owner             =>  $title,
+      group             =>  $title,
+      mode              =>  0750,
+      require           =>  [ User[$title], Group[$title] ],
+    }
+
+    ssh_authorized_key { $title:
+      target => "/software/ssh/${title}/authorized_keys",
+      ensure => present,
+      name   => $title,
+      user   => $title,
+      type   => $sshKeyType,
+      key    => $sshKey,
+      require => File["/home/${title}/.ssh"],
+    }
   }
 }
